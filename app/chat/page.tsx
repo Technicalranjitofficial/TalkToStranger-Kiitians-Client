cfb56d122ddfb8c4b0a7142ec5858628c0fe283a
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-11-02 18:57:14 +0530

    Talk with Stranger

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 import ChatCard from "@/components/ChatCard";
 import { useAppSelector } from "@/redux/hooks";
-import { checkSocketConnection, handleOnChatJoin, handleOnFindOtherUser, handleOnSendMessage, initSocket } from "@/socket/socket";
+import { Message } from "@/redux/slice/SocketSlice";
+import {
+  checkSocketConnection,
+  handleOnChatJoin,
+  handleOnFindOtherUser,
+  handleOnSendMessage,
+  initSocket,
+} from "@/socket/socket";
+import { AnimatePresence } from "framer-motion";
 import { useSession } from "next-auth/react";
 import React, { use, useEffect, useRef, useState } from "react";
+
+
+
+
+
 
 const Page = () => {
   const [isConnected, setIsConnected] = useState(false);
@@ -11,12 +24,8 @@ const Page = () => {
   const [socketCheck, setSocketCheck] = useState<boolean>(false);
   const messageRef = useRef<HTMLInputElement>(null);
 
-  const remoteuser = useAppSelector((state)=>state.socketSlice.remoteUsersId);
-
-  const Message = useAppSelector((state)=>state.socketSlice.messages);
-
- 
-
+  const remoteuser = useAppSelector((state) => state.socketSlice.remoteUsersId);
+  const Message = useAppSelector((state) => state.socketSlice.messages);
   useEffect(() => {
     if (session.data) {
       const res = initSocket(session.data);
@@ -26,86 +35,88 @@ const Page = () => {
     }
   }, [session]);
 
-  useEffect(()=>{
+ 
+  useEffect(() => {
     //handle on enter key pressed
-    const handleOnEnterKeyPressed = (e:KeyboardEvent)=>{
-      if(e.key=="Enter"){
+    const handleOnEnterKeyPressed = (e: KeyboardEvent) => {
+      if (e.key == "Enter") {
         handleOnSendMessage(messageRef.current?.value);
-        messageRef.current!.value="";
-        
-        messageRef.current?.blur();
+        messageRef.current!.value = "";
+        messageRef.current?.focus();
       }
-    }
-    document.addEventListener("keydown",handleOnEnterKeyPressed);
-    return ()=>document.removeEventListener("keydown",handleOnEnterKeyPressed);
-  },[])
+    };
+    document.addEventListener("keydown", handleOnEnterKeyPressed);
+    return () =>
+      document.removeEventListener("keydown", handleOnEnterKeyPressed);
+  }, []);
 
-
-
-  const scrollRef= useRef<HTMLDivElement>(null)
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
   // components/MessageList.tsx
 
+  useEffect(() => {
+    if (Message.length) {
 
-useEffect(() => {
-  if (Message.length) {
-    scrollRef.current?.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-    });
-  }
-}, [Message.length]);
+      if(messagesContainerRef.current){
+        messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+      }
+    }
+  }, [Message.length]);
 
+  if (!session) return <h1>Please Logged IN</h1>;
 
-if(!session) return <h1>Please Logged IN</h1>;
+  return (
+    <main className="flex  relative h-screen overflow-y-hidden  pb-5 flex-col mx-auto max-w-4xl">
+     
+      <div className="w-full overflow-y-hidden h-full pt-16 z-0  bg-[#0D1116]">
 
-  
+      {remoteuser && Message.length==0 ? 
+      
+      <div className="w-full h-full flex justify-center mt-5">
+        <h1 className="text-white">Stranger Connected</h1>
+      </div>
+      :Message.length>0?  
+        <div ref={messagesContainerRef} className="overflow-y-auto px-2   scrollbar-thin scrollbar-thumb-cyan-600 w-full mt-2 lg:h-[90%] h-[80%]">
+          <AnimatePresence>
 
-  return(
-    <main className="flex min-h-screen overflow-y-hidden h-screen pb-5 flex-col  pt-16 mx-auto max-w-4xl">
-
-      <div className="w-full h-full z-0 relative bg-[#0D1116]">
-
-        {(remoteuser && Message.length==0)? <div className="flex text-white flex-col items-center justify-center">
-          <h1 className="text-xl font-bold">Stranger Connected</h1>
-        
-        </div>: Message.length>0? null: <div className="flex text-white flex-col items-center justify-center">
-          <h1 className="text-xl font-bold">Waiting for Stranger</h1>
-          {/* <button onClick={handleOnFindOtherUser} className="px-2 py-1 bg-transparent border border-gray-800 mt-2">Find Other</button> */}
+         
+          {Message.length > 0 &&
+            Message.map((message, index) => {
+              return <ChatCard key={index} message={message} />;
+            })}
+   
+   </AnimatePresence>
+    
+        </div>: <div className="w-full h-full flex justify-center items-center">
+          <h1 className="text-white">Waiting for user connection</h1>
         </div>
-          }
+          
+           }
 
-       <div className="overflow-y-auto px-2 scrollbar-thin scrollbar-thumb-cyan-600 w-full mt-2 h-[80%]">
-       {
-        Message.length>0 &&  Message.map((message,index)=>{
-            return(
-              <ChatCard key={index} message={message}/>
-            )
-          })
-
-       }
-
-        <div ref={scrollRef}></div>
-       </div>
-
-        <div className="absolute w-full px-2  gap-5 flex justify-evenly flex-row bottom-12">
-          <input  ref={messageRef} className="w-full px-2 bg-transparent border border-gray-800 text-white outline-none rounded-md py-2" type="text" />
-          <button disabled={remoteuser==null} onClick={()=>{
-            handleOnSendMessage(messageRef.current?.value);
-           
-              scrollRef.current?.scrollIntoView({
-                behavior: "smooth",
-                block: "end",
-              });
-              messageRef.current!.value="";
-              messageRef.current?.blur();
-            
-          }} className="w-1/4 bg-transparent text-white border border-gray-700 rounded-md">Send</button>
+        <div className="absolute w-full px-2 pb-5  gap-5 flex justify-evenly flex-row bottom-12 md:bottom:10 lg:bottom-5">
+          <input
+          disabled={remoteuser == null}
+            ref={messageRef}
+            className="w-full px-2 bg-transparent border border-gray-800 text-white outline-none rounded-md py-2"
+            type="text"
+          />
+          <button
+            disabled={remoteuser == null}
+            onClick={() => {
+              handleOnSendMessage(messageRef.current?.value);
+                   
+                      messageRef.current!.value = "";
+                      messageRef.current?.focus();            
+              
+            }}
+            className="w-1/4 bg-transparent text-white border border-gray-700 rounded-md"
+          >
+            Send
+          </button>
         </div>
       </div>
-   
     </main>
-  )
+  );
 };
 
 export default Page;
-
--- components/ChatCard.tsx
+++ components/ChatCard.tsx
@@ -2,6 +2,8 @@ import { Message } from '@/redux/slice/SocketSlice'
 import { Merriweather, Montserrat, Roboto } from 'next/font/google'
 import React from 'react'
 
+import {motion} from "framer-motion"
+
 const roboto = Merriweather({
     weight: '900',
     subsets:['cyrillic']
@@ -13,7 +15,9 @@ const roboto = Merriweather({
 
 const ChatCard = ({message}:{message:Message}) => {
   return (
-    <div className={` ${message.isSender?"justify-end":""} mt-5 h-auto flex `}>
+    <motion.div initial={{opacity:0,scale:0.8}} animate={{opacity:1,scale:1}} transition={{opacity:{duration:0.2}}} style={{
+      originX:message.isSender?1:0,
+    }} className={` ${message.isSender?"justify-end":""} mt-5 h-auto flex `}>
        <div className='w-auto min-w-[100px] py-3 px-5 rounded-md bg-transparent border border-gray-800 flex flex-col '>
        <div>
             <h1 style={roboto.style} className='text-gray-400'>{message.isSender?"Me":"Stranger"}</h1>
@@ -26,7 +30,7 @@ const ChatCard = ({message}:{message:Message}) => {
             </div>
 
        </div>
-    </div>
+    </motion.div>
   )
 }
 
